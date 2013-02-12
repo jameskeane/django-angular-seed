191538890121105873c7acbfc06786ee773f250a
James Keane <james@bnotions.com>
2013-02-12 15:56:44 -0500

    Fixes django-compressor deployment issues.

--- app/settings.py
+++ app/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'dist'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -144,6 +144,7 @@ COMPRESS_PRECOMPILERS = (
     ('text/stylus', 'stylus < {infile} > {outfile}'),
     ('text/foobar', 'path.to.MyPrecompilerFilter'),
 )
+COMPRESS_URL = STATIC_URL
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
