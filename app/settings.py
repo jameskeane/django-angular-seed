644f4aafc82b1c7eb49a25fe624b44378d0ebc9f
James Keane <james@bnotions.com>
2013-02-12 23:58:34 -0500

    Changes for heroku to pick up static files.

--- app/settings.py
+++ app/settings.py
@@ -2,6 +2,7 @@ import os
 
 # Django settings for hammer project.
 ENV = os.environ.get('ENV', 'local').lower()
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False if ENV == 'production' else True
 TEMPLATE_DEBUG = DEBUG
@@ -59,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(os.getcwd(), 'dist/')
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../dist/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
