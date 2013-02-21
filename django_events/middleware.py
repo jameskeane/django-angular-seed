995e8b1e3aa28a4a7ce4f55ea92b1b77d4572ba5
James Keane <james.keane.github@gmail.com>
2013-02-20 22:54:41 -0500

    The WSGIHandler now applies middleware :D

--- django_events/middleware.py
+++ django_events/middleware.py
@@ -2,6 +2,7 @@ import time
 from django.conf import settings
 from django.http import HttpResponse
 from django_events import GEVENT_ENABLED, EVENTS_RESOURCE
+import json
 
 class EventsMiddleware(object):
     def process_request(self, request):
@@ -13,6 +14,7 @@ class EventsMiddleware(object):
 
     def handle_stream(self, request, start_response):
         start_response("200", [('Content-Type', 'text/event-stream')])
+        yield json.dumps(request.user.is_authenticated())
         yield "3"
         time.sleep(10)
         yield "4"
--- django_events/wsgi_patch.py
+++ django_events/wsgi_patch.py
@@ -1,10 +1,15 @@
-from django.core.handlers.wsgi import WSGIRequest, WSGIHandler
+from django.core.handlers.wsgi import WSGIRequest, WSGIHandler, STATUS_CODE_TEXT
+from django.core.handlers import base
+from django.core import signals
+from django.core.urlresolvers import set_script_prefix
+
 from django_events.middleware import EventsMiddleware
 from django_events import GEVENT_ENABLED, EVENTS_RESOURCE
 
 class EventsWSGIHandler(WSGIHandler):
     def __init__(self, application):
         self.application = application
+        super(EventsWSGIHandler, self).__init__()
 
     def __call__(self, environ, start_response):
         path = environ.get('PATH_INFO')
@@ -13,5 +18,47 @@ class EventsWSGIHandler(WSGIHandler):
 
         # We are making a data stream
         # first, grab the request object
-        request = WSGIRequest(environ)
-        return EventsMiddleware().handle_stream(request, start_response)
+
+        # Set up middleware if needed. We couldn't do this earlier, because
+        # settings weren't available.
+        if self._request_middleware is None:
+            self.initLock.acquire()
+            try:
+                try:
+                    # Check that middleware is still uninitialised.
+                    if self._request_middleware is None:
+                        self.load_middleware()
+                except:
+                    # Unload whatever middleware we got
+                    self._request_middleware = None
+                    raise
+            finally:
+                self.initLock.release()
+
+
+        set_script_prefix(base.get_script_name(environ))
+        signals.request_started.send(sender=self.__class__)
+        
+        # TODO: error handling, see: django.core.handlers.wsgi.WSGIHandler
+        request = self.request_class(environ)
+        response = None
+
+        # Apply request middleware
+        for middleware_method in self._request_middleware:
+            response = middleware_method(request)
+            if response:
+                break
+
+        if response is None:
+            return EventsMiddleware().handle_stream(request, start_response)
+        else:
+            try:
+                status_text = STATUS_CODE_TEXT[response.status_code]
+            except KeyError:
+                status_text = 'UNKNOWN STATUS CODE'
+            status = '%s %s' % (response.status_code, status_text)
+            response_headers = [(str(k), str(v)) for k, v in response.items()]
+            for c in response.cookies.values():
+                response_headers.append(('Set-Cookie', str(c.output(header=''))))
+            start_response(status, response_headers)
+            return response
