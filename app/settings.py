d1a61015c8fe5ccc5988e0be38252b76d031d127
James Keane <james@bnotions.com>
2013-03-07 18:29:20 -0500

    Fixes the django-waffle caching issue, just use a dummy cache.

--- app/settings.py
+++ app/settings.py
@@ -24,6 +24,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
